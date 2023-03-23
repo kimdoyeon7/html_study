002f077a842b183e4ab88c4052a2482788c55341
DO YEON <125236689+kimdoyeon7@users.noreply.github.com>
2023-03-23 23:59:29 +0900

    Add files via upload

--- javascript_start/script/js2-1.js
+++ javascript_start/script/js2-1.js
@@ -1,1 +1,18 @@
-// 
+// answer_btn, number, data_type, result2
+
+
+const result = document.querySelector('#result')
+const data_type = document.querySelector('#result1')
+const user_answer = document.querySelector('#result2')
+const answer_btn = document.querySelector('#answer_btn')
+
+
+console.log(data_type, user_answer, answer_btn)
+
+
+answer_btn.addEventListener('click',function(){
+    let temp = Number(result.value);
+    // console.log('this')
+    data_type.value = typeof (temp)
+    user_answer.value = temp
+})
